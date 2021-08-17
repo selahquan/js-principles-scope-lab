519442cb49ad4ef3615fd5e4e965fd49d7717dca
Josh <josh.handon@gmail.com>
2021-08-17 18:37:42 -0400

    commit tree b39b3590ff2c8c0cc260176baede297676cec4da

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer = 'Phylis'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = bestCustomer = "maybe bob"
+}
+
+
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "Deyoung"
+}
